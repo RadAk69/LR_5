c0cef49bd7e83528b0a91b83ab444dd5724e6e86
RadAk69 <lesaisakov85@gmail.com>
2026-09-25 16:01:04 +0500

    Add files via upload

---
+++ Задание 1.py
@@ -0,0 +1,16 @@
+a=int(input("введите первеое число:"))
+b=int(input("введите второе чилсо:"))
+num1 = a+b
+num2 = a-b
+num3 = a*b
+num4 = a/b
+num5 = a//b
+num6 = a%b
+num7 = a**b
+print(num1)
+print(num2)
+print(num3)
+print(num4)
+print(num5)
+print(num6)
+print(num7)
